8bb9738d5cb22734522893711f0d7a285752f36e
Amanda <amanda.simoes1707@gmail.com>
2021-10-03 11:28:35 -0300

    Importaçao das pages na navegaçao

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,6 +5,12 @@ import { createStackNavigator } from '@react-navigation/stack';
 import {Login} from './pages/Login/index';
 import { Start } from './pages/Start';
 import { CadastroOne } from "./pages/CadastroOne";
+import { CadastroTwo } from './pages/CadastroTwo';
+import { CupomAberto } from './pages/CupomAberto';
+import { Cupons } from './pages/Cupons';
+import { EditarPerfil } from './pages/EditarPerfil';
+import { Home } from './pages/Home';
+import { Perfil } from './pages/Perfil';
 
 const {Navigator, Screen} = createStackNavigator();
 const Routes: React.FC = () => {
@@ -15,6 +21,12 @@ const Routes: React.FC = () => {
                 <Screen name="Start" component={Start}/>
                 <Screen name="Login" component={Login} />
                 <Screen name="CadastroOne" component={CadastroOne} />
+                <Screen name="CadastroTwo" component={CadastroTwo}/>
+                <Screen name="CupomAberto" component={CupomAberto}/>
+                <Screen name="Cupons" component={Cupons}/>
+                <Screen name="EditarPerfil" component={EditarPerfil}/>
+                <Screen name="Home" component={Home}/>
+                <Screen name="Perfil" component={Perfil}/>
             </Navigator>
         </NavigationContainer>
 
